7b6d70a3831d76b4011a142a4989445b473c67f3
Yukti <yukti.abrol@gmail.com>
2015-03-07 14:39:37 -0800

    back to yukti's version

--- lab2/sample.c
+++ lab2/sample.c
@@ -4,40 +4,14 @@
 #include "mw_api.h"
 
 struct work_t {
-	double t; 
+	int t; 
 };
 
 struct result_t {
-   	double t;
+   	int t;
 };
 
 work_unit** create_work(int argc, char **argv){
-<<<<<<< HEAD
-	int size =6;
-	printf("%d\n",size);
-	work_unit** t=(work_unit**)malloc(size*sizeof(work_unit*)); 
-	int i=0;	
-	for(i=0;i<size-1;i++){
-		work_unit* temp = (work_unit*)malloc(sizeof(work_unit));
-		temp->t = (double)i;
-		t[i]=temp;
-	}
-	t[size-1]=NULL;
-	printf("created_work\n");
-	return t;
-}
-
-int process_results(int sz, result_unit **res){
-double result=0;
-int i=0;
-for(i=0;i<sz;i++)
-{result += res[i]->t;
-
-printf("result %f %f %d\n",res[i]->t,result,sz);
-}
-printf("%f\n",result);
-return result;
-=======
 	int size =10;
 	work_unit** t=(work_unit**)malloc(size*sizeof(work_unit*)); //array
 	int i=0;	
@@ -54,16 +28,10 @@ int process_results(int sz, result_unit *res){
 	result = res->t;
 	printf("%d\n",result);
 	return result;
->>>>>>> 366fd29bdb8b899ba28fe5e117af0fea4b7d6cce
 }
 
 result_unit* do_work(work_unit *work){
 	result_unit* res = (result_unit *)malloc(sizeof(result_unit));
-<<<<<<< HEAD
-	res->t= work->t;
-	printf("doing work %f\n",res->t);
-	return res;
-=======
 	res->t=work->t;
 	return res;
 }
@@ -76,25 +44,23 @@ double dotProduct(double *a, double *b, int length) {
 	for (index = 0; index < length; index++)
 	runningSum += a[index] * b[index];
 	return runningSum;
->>>>>>> 366fd29bdb8b899ba28fe5e117af0fea4b7d6cce
 }
 
 int main (int argc, char **argv)
 {
   struct mw_api_spec f;
 
+  //MPI_Init (&argc, &argv);
 
   f.create = create_work;
   f.compile = process_results;
   f.compute = do_work;
   f.work_sz = sizeof (work_unit);
   f.res_sz = sizeof (result_unit);
-
-  MPI_Init (&argc, &argv);
   MW_Run (argc, argv, &f);
-  MPI_Finalize ();
-  
   testing();
+  //MPI_Finalize ();
+  
   return 0;
 
 }
