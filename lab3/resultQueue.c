7f2d1dba368bf188a452f7d06814f52faca50b7d
Yukti <yukti.abrol@gmail.com>
2015-03-23 17:15:33 -0400

    fixed up result queue methods

--- lab3/resultQueue.c
+++ lab3/resultQueue.c
@@ -27,25 +27,30 @@ int rQueue_empty(result_queue queue){
 	return FALSE;
 }
 
-void rDequeue(result_queue queue)
+result_node *rDequeue(result_queue queue)
 {
 	if(queue == NULL){
 		printf("Queue Pointer Empty");
-		return;
+		return NULL;
 	}
 	if(rQueue_empty(queue) == TRUE){
 		printf("Queue Empty");
-		return;
+		return NULL;
 	}
 	else{
 		//check for one element
 		if(queue->front == queue->rear){
+      result_node *temp = queue->front;
 			queue->front = NULL;
 			queue->rear = NULL;
-			return;
+      temp->next = NULL;
+			return temp;
 		}
 		else{
+      result_node *temp = queue->front;
 			queue->front = (queue->front)->next;
+      temp->next = NULL;
+      return temp;
 		}
 	}
 
@@ -73,8 +78,7 @@ void rQueue_destroy(result_queue queue)
   free(queue);
 }
 
-void rEnqueue(result_queue queue, result_unit *result, int id)
-{
+result_node *rGet_result_node(result_unit *result, int id){
   result_node *new_result;
 
   /* Allocate space for a node in the linked list. */
@@ -85,8 +89,69 @@ void rEnqueue(result_queue queue, result_unit *result, int id)
   }
   /* Place information in the node */
   new_result->result = result;
+  new_result->id = id;
   new_result->next = NULL;
-  new_result->workID = id;
+  return new_result;
+}
+
+result_node *rDequeue_by_id(result_queue queue, int id){
+  if(queue == NULL){
+    printf("Queue Pointer Empty");
+    return NULL;
+  }
+  if(rQueue_empty(queue) == TRUE){
+    printf("Queue Empty");
+    return NULL;
+  }
+  else{
+    //check for one element
+    result_node *ptr = queue->front;
+    result_node *prev = NULL;
+    while(ptr!=NULL){
+      if(ptr->id == id){
+          //check for one element
+          if(queue->front == queue->rear){
+            result_node *temp = queue->front;
+            queue->front = NULL;
+            queue->rear = NULL;
+            temp->next = NULL;
+            return temp;
+          }
+          else{
+            if(ptr == queue->front){
+              result_node *temp;
+              temp = queue->front;
+              queue->front = queue->front->next;
+              temp->next = NULL;
+              return temp;
+            }
+            else if(ptr == queue->rear)
+            {
+              result_node *temp = ptr;
+              queue->rear = prev;
+              prev->next = NULL;
+              temp->next = NULL;
+              return temp;
+            }
+            else{
+              result_node *temp = ptr;
+              prev->next = ptr->next;
+              temp->next = NULL;
+              return temp;
+            }
+          } 
+      }
+      else{
+        prev = ptr;
+        ptr = ptr->next;
+      }
+    }
+    return prev;
+  }
+}
+
+void rEnqueue(result_queue queue, result_node *new_result)
+{
   /*
    * Link the element into the right place in
    * the linked list.
--- lab3/resultQueue.h
+++ lab3/resultQueue.h
@@ -3,7 +3,7 @@
 
 typedef struct result_node_t{
    result_unit *result;
-   int workID;
+   int id;
    struct result_node_t *next;
 } result_node;
 
@@ -16,8 +16,10 @@ typedef struct result_queue_node_t
 
 result_queue rQueue_create(void);
 int rQueue_empty(result_queue queue);
-void rDequeue(result_queue queue);
+result_node *rDequeue(result_queue queue);
 void rQueue_destroy(result_queue queue);
-void rEnqueue(result_queue queue, result_unit *result, int id);
+void rEnqueue(result_queue queue, result_node *new_result);
+result_node *rGet_result_node(result_unit *result, int id);
+result_node *rDequeue_by_id(result_queue queue, int id);
 
 #endif
